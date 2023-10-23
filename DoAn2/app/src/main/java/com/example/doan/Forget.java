6b83a2a63eafa0356c45c1f3dcf5fe76c5b0ba8b
Shiroizdabezt <codykhoanguyen@gmail.com>
2023-10-23 13:38:05 +0700

    UI for forget pass and sign up

--- DoAn2/app/src/main/java/com/example/doan/Forget.java
+++ DoAn2/app/src/main/java/com/example/doan/Forget.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Forget extends AppCompatActivity {
 
@@ -16,5 +24,23 @@ public class Forget extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Forget Password");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner_SecurityQuestion);
+
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("Item 1");
+        categories.add("Item 2");
+        categories.add("Item 3");
+        categories.add("Item 4");
+        categories.add("Item 5");
+        categories.add("Item 6");
+
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
     }
 }
