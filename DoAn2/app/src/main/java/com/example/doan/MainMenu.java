73ec072a3894fe0809ff61d309d9193d75520cc3
Shiroizdabezt <codykhoanguyen@gmail.com>
2023-10-28 02:25:25 +0700

    test

--- DoAn2/app/src/main/java/com/example/doan/MainMenu.java
+++ DoAn2/app/src/main/java/com/example/doan/MainMenu.java
@@ -1,30 +1,124 @@
 package com.example.doan;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
+
+    FloatingActionButton floatingActionButton;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        ImageButton imbMenu = (ImageButton) findViewById(R.id.imgbtn_menu);
-        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        //NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
-        imbMenu.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar;
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open, R.string.navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_drawer);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setBackground(null);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                drawerLayout.open();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int ItemID = item.getItemId();
+                if(ItemID == R.id.bottom_vocab)
+                {
+                    openFragment(new Ton());
+                    return true;
+                }
+                else if(ItemID == R.id.bottom_grammar)
+                {
+                    openFragment(new Thien());
+                    return true;
+                }
+                else if(ItemID == R.id.bottom_test)
+                {
+                    openFragment(new Ban());
+                    return true;
+                }
+                return false;
             }
         });
 
+        fragmentManager = getSupportFragmentManager();
+        openFragment(new Home());
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainMenu.this, "Tốn liền", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+        if(itemID == R.id.Ton)
+        {
+            openFragment(new Ton());
+        }
+        else if(itemID == R.id.Thien)
+        {
+            openFragment(new Thien());
+        }
+        else if(itemID == R.id.Ban)
+        {
+            openFragment(new Ban());
+        }
+        return false;
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    private void openFragment(Fragment fragment)
+    {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.framelayout_container, fragment);
+        transaction.commit();
 
     }
 }
