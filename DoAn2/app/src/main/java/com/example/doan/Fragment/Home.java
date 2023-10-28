112ce89cd4c36430317e0b4ec0aa0db2987a521d
Shiroizdabezt <codykhoanguyen@gmail.com>
2023-10-28 23:59:54 +0700

    test

--- DoAn2/app/src/main/java/com/example/doan/Fragment/Home.java
+++ DoAn2/app/src/main/java/com/example/doan/Fragment/Home.java
@@ -1,4 +1,4 @@
-package com.example.doan;
+package com.example.doan.Fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.doan.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Thien#newInstance} factory method to
+ * Use the {@link Home#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Thien extends Fragment {
+public class Home extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class Thien extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Thien() {
+    public Home() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class Thien extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Thien.
+     * @return A new instance of fragment Home.
      */
     // TODO: Rename and change types and number of parameters
-    public static Thien newInstance(String param1, String param2) {
-        Thien fragment = new Thien();
+    public static Home newInstance(String param1, String param2) {
+        Home fragment = new Home();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class Thien extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thien, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
--- DoAn2/app/src/main/java/com/example/doan/MainMenu.java
+++ DoAn2/app/src/main/java/com/example/doan/MainMenu.java
@@ -1,124 +1,117 @@
 package com.example.doan;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
+import android.view.Menu;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.example.doan.Fragment.Home;
+import com.example.doan.Fragment.Setting;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    FloatingActionButton fab;
     DrawerLayout drawerLayout;
     BottomNavigationView bottomNavigationView;
-
-    FloatingActionButton floatingActionButton;
+    MenuItem home, setting;
     FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        Toolbar toolbar;
-        toolbar = findViewById(R.id.toolbar);
+
+        home = findViewById(R.id.nav_home);
+        setting = findViewById(R.id.nav_settings);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open, R.string.navigation_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.navigation_drawer);
-        navigationView.setNavigationItemSelectedListener(this);
+        if(savedInstanceState == null )
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new Home()).commit();
+            navigationView.setCheckedItem(R.id.nav_view);
+        }
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        /*replaceFragment(new Home());
+
         bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
 
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int ItemID = item.getItemId();
-                if(ItemID == R.id.bottom_vocab)
-                {
-                    openFragment(new Ton());
-                    return true;
-                }
-                else if(ItemID == R.id.bottom_grammar)
-                {
-                    openFragment(new Thien());
-                    return true;
-                }
-                else if(ItemID == R.id.bottom_test)
-                {
-                    openFragment(new Ban());
-                    return true;
-                }
-                return false;
+            switch (item.getItemId()) {
+                case R.id.Test:
+                    replaceFragment(new Home());
+                    break;
+                case R.id.grammar:
+                    replaceFragment(new Setting());
+                    break;
+
             }
-        });
 
-        fragmentManager = getSupportFragmentManager();
-        openFragment(new Home());
+            return true;
+        });*/
 
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainMenu.this, "Tốn liền", Toast.LENGTH_SHORT).show();
-            }
-        });
+    }
+
+    private  void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
         int itemID = item.getItemId();
-        if(itemID == R.id.Ton)
+        if(itemID == R.id.nav_home)
         {
-            openFragment(new Ton());
+            openFragment(new Home());
         }
-        else if(itemID == R.id.Thien)
+        else if(itemID == R.id.nav_settings)
         {
-            openFragment(new Thien());
-        }
-        else if(itemID == R.id.Ban)
-        {
-            openFragment(new Ban());
+            openFragment(new Setting());
         }
         return false;
     }
 
-    @Override
-    public void onBackPressed()
-    {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else {
-            super.onBackPressed();
-        }
-    }
-
-    private void openFragment(Fragment fragment)
-    {
+    private void openFragment(Fragment fragment){
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.framelayout_container, fragment);
+        transaction.replace(R.id.frame_layout, fragment);
         transaction.commit();
-
     }
 }
