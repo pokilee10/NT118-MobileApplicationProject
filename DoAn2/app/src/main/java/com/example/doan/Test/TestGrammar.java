ae013e41a3c7a00aca99484806b2e67426c25593
lexuanlinh <troilanhnuc@gmail.com>
2023-11-12 01:42:32 +0700

    update UI

--- DoAn2/app/src/main/java/com/example/doan/Test/TestGrammar.java
+++ DoAn2/app/src/main/java/com/example/doan/Test/TestGrammar.java
@@ -4,20 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.doan.MainMenu;
 import com.example.doan.MainMenuFolder.Test;
 import com.example.doan.R;
 
+import java.util.Locale;
+
 public class TestGrammar extends AppCompatActivity {
+
+    ImageButton imgbtn_back;
+    TextView tvQuesTime;
+    CountDownTimer count;
+    ProgressBar progressBarQuesTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testgrammar);
-        ImageButton imgbtn_back = (ImageButton) findViewById(R.id.imgbtn_back);
+
+        imgbtn_back = findViewById(R.id.imgbtn_back);
+        tvQuesTime = findViewById(R.id.textViewQuesTime);
+        progressBarQuesTime = findViewById(R.id.progressBarQuesTime);
+        startTime();
+        count.start();
+
         imgbtn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,7 +43,26 @@ public class TestGrammar extends AppCompatActivity {
         });
     }
 
-    public void Back(){
+    private void startTime() {
+        count = new CountDownTimer(30000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long seconds = (millisUntilFinished / 1000) % 60;
+                String timeFormatted = String.format(Locale.getDefault(), "%02d", seconds);
+                tvQuesTime.setText(timeFormatted + "s");
+                int current = progressBarQuesTime.getProgress();
+                progressBarQuesTime.setProgress(current - 10);
+            }
+
+            @Override
+            public void onFinish() {
+                tvQuesTime.setText("0s");
+                Toast.makeText(TestGrammar.this, "Time's up", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    public void Back() {
         Intent intent = new Intent(this, Test.class);
         startActivity(intent);
     }
--- DoAn2/app/src/main/java/com/example/doan/Test/TestVocabulary.java
+++ DoAn2/app/src/main/java/com/example/doan/Test/TestVocabulary.java
@@ -4,25 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.doan.MainMenuFolder.Test;
 import com.example.doan.R;
 
+import java.util.Locale;
+
 public class TestVocabulary extends AppCompatActivity {
 
+    TextView tvQuesTime;
+    CountDownTimer count;
+    ProgressBar progressBarQuesTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_vocabulary);
         ImageButton imgbtn_back = (ImageButton) findViewById(R.id.imgbtn_back);
+
+        tvQuesTime = findViewById(R.id.textViewQuesTime);
+        progressBarQuesTime = findViewById(R.id.progressBarQuesTime);
+        startTime();
+        count.start();
         imgbtn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Back();
             }
         });
+    }
+
+    private void startTime() {
+        count = new CountDownTimer(30000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long seconds = (millisUntilFinished / 1000) % 60;
+                String timeFormatted = String.format(Locale.getDefault(), "%02d", seconds);
+                tvQuesTime.setText(timeFormatted + "s");
+                int current = progressBarQuesTime.getProgress();
+                progressBarQuesTime.setProgress(current - 10);
+            }
+
+            @Override
+            public void onFinish() {
+                tvQuesTime.setText("0s");
+                Toast.makeText(TestVocabulary.this, "Time's up", Toast.LENGTH_SHORT).show();
+            }
+        };
     }
 
     public void Back(){
