ecdcbde5541458acdab6198944918d9d057c54aa
lexuanlinh <troilanhnuc@gmail.com>
2023-11-12 17:32:28 +0700

    update UI

--- DoAn2/app/src/main/java/com/example/doan/GrammarCau/GrammarCau1_1.java
+++ DoAn2/app/src/main/java/com/example/doan/GrammarCau/GrammarCau1_1.java
@@ -7,35 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-import com.example.doan.MainMenu;
 import com.example.doan.MainMenuFolder.GrammarCau;
 import com.example.doan.R;
 
-public class GrammarCau1 extends AppCompatActivity {
+public class GrammarCau1_1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grammar_cau1);
+        setContentView(R.layout.activity_grammar_cau11);
         ImageButton imgbtn_back = (ImageButton) findViewById(R.id.imgbtn_back);
-        ImageButton imgbt1 = (ImageButton) findViewById(R.id.ibt1);
         imgbtn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Back();
             }
         });
-        imgbt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(GrammarCau1.this, GrammarCau1_1.class);
-                startActivity(intent);
-            }
-        });
     }
-
     public void Back(){
-        Intent intent = new Intent(this, GrammarCau.class);
+        Intent intent = new Intent(this, GrammarCau1.class);
         startActivity(intent);
     }
 }
