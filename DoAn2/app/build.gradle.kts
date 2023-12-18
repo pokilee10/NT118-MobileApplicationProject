3ec480268009793f5331cc77f50e51b81844790e
Shiroizdabezt <codykhoanguyen@gmail.com>
2023-12-18 13:31:10 +0700

    Create Database

--- DoAn2/app/build.gradle.kts
+++ DoAn2/app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.7.4")
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
